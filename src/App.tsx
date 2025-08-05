ed483c43f067902a819bf4dfcc54ffce2aa24f17
youlethal <78674543+youlethal@users.noreply.github.com>
2025-08-05 01:21:34 +0100

    Update src\App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { defaultConfig, applyBranding } from './config';
+import TemplateLayout from './components/layout/TemplateLayout';
+
+function App() {
+  useEffect(() => {
+    // Apply branding on mount
+    applyBranding(defaultConfig.branding);
+  }, []);
+
+  return (
+    <TemplateLayout 
+      branding={defaultConfig.branding}
+      modules={defaultConfig.modules}
+      darkMode={defaultConfig.darkMode}
+    />
+  );
+}
+
+export default App; 
